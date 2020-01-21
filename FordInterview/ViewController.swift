36d2a5e2a06dcb5ec3dde8f65ffdf35667ffd58b
Mike Gopsill <m.gopsill@gmail.com>
2020-01-21 07:28:58 +0000

    Initial Commit

---
+++ FordInterview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FordInterview
+//
+//  Created by Mike Gopsill on 21/01/2020.
+//  Copyright © 2020 mgopsill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
