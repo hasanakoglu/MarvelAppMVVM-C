895744b951f2ad47d37cc1a681a1c5e524b0a2f5
Mike Gopsill <m.gopsill@gmail.com>
2020-01-21 08:04:09 +0000

    Starting Point

--- FordInterview/MarvelURLs.swift
+++ FordInterview/MarvelURLs.swift
@@ -11,26 +11,3 @@ import Foundation
 enum API {
     static let charactersURL = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=ff3d4847092294acc724123682af904b&hash=412b0d63f1d175474216fadfcc4e4fed&limit=25&orderBy=-modified"
 }
-
-
-func fetch() {
-    let url = URL(string: API.charactersURL)!
-    let request = URLRequest(url: url)
-    let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
-
-        print("📡📡📡 Fetch completed")
-        print("💾💾💾 Data = \(String(describing: data))")
-        print("🗣🗣🗣 Response = \(String(describing: response))")
-        print("🆘🆘🆘 Error = \(String(describing: error))")
-
-        if let data = data {
-            if let model = try? JSONDecoder().decode(CharacterResponseModel.self, from: data) {
-                print("✅✅✅ Data Decoded")
-                print("🦾🦾🦾 model.data.characters = \(String(describing: model.data.characters))")
-            } else {
-                print("🥴🥴🥴 Error: Data not decoded properly")
-            }
-        }
-    }
-    dataTask.resume()
-}
